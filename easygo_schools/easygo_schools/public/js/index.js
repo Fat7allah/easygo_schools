52dc09cef2bc8e77ca3d50f0c594f11ce3f0c63c
Fat7allah <yassine.fat7allah@gmail.com>
2025-09-03 21:14:18 +0100

    fix install erros

--- easygo_schools/easygo_schools/public/js/index.js
+++
@@ -1,8 +0,0 @@
-// EasyGo Education - Main entry point for assets
-// This file serves as the entry point for esbuild
-
-// Import main app functionality
-import './easygo_education.js';
-
-// Export for esbuild compatibility
-export default {};
